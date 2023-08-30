e462473fd97fb7938988db717e53c91ca466a1d0
jamesdanielwhitford <james.whitford0@gmail.com>
2023-08-30 19:30:59 +0200

    Add build

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Upload from './Upload';
+// import Upload from './Upload';
 import Display from './Display';
 import LandingPage from './LandingPage';
 
 function App() {
   return (
     <div className="app-container">
-      <Upload />
+      {/* <Upload /> */}
       <LandingPage />
       <Display />
     </div>
