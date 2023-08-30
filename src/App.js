ee5584ee040dfdeb64b3f0739b7c8482743f169b
jamesdanielwhitford <james.whitford0@gmail.com>
2023-08-30 14:20:01 +0200

    Good styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Upload from './Upload';
 import Display from './Display';
+import LandingPage from './LandingPage';
 
 function App() {
   return (
-    <div>
+    <div className="app-container">
       <Upload />
+      <LandingPage />
       <Display />
     </div>
   );
---
+++ src/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function LandingPage() {
+  return (
+    <div className="landing-container">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.</p>
+    </div>
+  );
+}
+
+export default LandingPage;
